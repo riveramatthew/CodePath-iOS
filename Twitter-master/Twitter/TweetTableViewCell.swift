b9ca2969d1ce1f18530dfd97b635cf68dff87b89
Matthew Rivera <61847038+riveramatthew@users.noreply.github.com>
2021-10-02 10:35:34 -0400

    Add files via upload

---
+++ Twitter-master/Twitter/TweetTableViewCell.swift
@@ -0,0 +1,80 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Edward de los Santos on 2/26/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var profileImageView: UIImageView!
+  @IBOutlet weak var userNameLabel: UILabel!
+  @IBOutlet weak var tweetContentLabel: UILabel!
+  @IBOutlet weak var retweetButton: UIButton!
+  @IBOutlet weak var favButton: UIButton!
+  
+  var favorited = false
+  var tweetId = -1
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+  
+  func setFavorite(_ isFavorited: Bool) {
+    favorited = isFavorited
+    if (favorited) {
+      favButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+    } else {
+      favButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
+    }
+  }
+  
+  
+  @IBAction func favoriteTweet(_ sender: Any) {
+    let toBeFavorited = !favorited
+    if (toBeFavorited) {
+      TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+        self.setFavorite(true)
+      }, failure: { (error) in
+        print("Favorite did not succeed: \(error)")
+      })
+    } else {
+      TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+        self.setFavorite(false)
+      }, failure: { (error) in
+        print("Unfavorite did not succeed: \(error)")
+      })
+    }
+  }
+  
+  
+  @IBAction func retweet(_ sender: Any) {
+    TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+      self.setRetweeted(true)
+    }, failure: { (error) in
+      print("Error is retweeting: \(error)")
+    })
+  }
+  
+  func setRetweeted(_ isRetweeted:Bool) {
+    if (isRetweeted) {
+      retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+      retweetButton.isEnabled = false
+    } else {
+      retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+      retweetButton.isEnabled = true
+    }
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
